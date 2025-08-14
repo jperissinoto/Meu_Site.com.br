3ad315677091047ac40d10b3ee8828876c62341c
joao pedro <joao.p.duarte12@aluno.senai.br>
2025-08-14 11:47:05 -0300

    laço e if

---
+++ laco_if.php
@@ -0,0 +1,22 @@
+<?php
+    $resposta ="não";
+    $pergunta ="Sua mãe deicou você ir ao MCdonald?";
+
+    if($resposta == "sim"){
+    echo "O rolê esta on";
+    }else{ 
+        echo "hoje não terá rolê";
+    }
+
+    echo "<br>"
+
+    $numero1 = 100;
+    $numero2 = 200;
+    if($numero1 >= $numero2){
+        echo "O $numero1 é maior ou igual que o $numero2";
+    }else{
+        echo "O $numero2 é maior ou igual que o $numero1";
+    }
+
+
+?>
