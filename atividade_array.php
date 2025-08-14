875336ca8e8e9b69bdad2c3a3168b8b8f8387da7
joao pedro <joao.p.duarte12@aluno.senai.br>
2025-08-14 15:39:35 -0300

    atividade

---
+++ atividade_array.php
@@ -0,0 +1,16 @@
+<?php
+    $frutas = [
+        "Maça",
+        "Banana",
+        "Morango",
+        "Pera",
+        "Laranja",
+        "Limão",
+    ];
+    echo $frutas[0]; // Maça
+
+    $y=4;
+    for($i = $y; $i <= 10; $i++){
+        echo "Número: $i <br>";
+    }
+?>
